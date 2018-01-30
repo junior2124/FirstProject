1a2be5b47a4e51960b2716c05769aa9c3aba9e94
junio <junio@DESKTOP-62SIQM5>
2018-01-29 23:19:08 -0600

    Add project files.

---
+++ FirstGitProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstGitProject.MvcApplication" Language="C#" %>
---
+++ FirstGitProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstGitProject.Startup))]
+namespace FirstGitProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
