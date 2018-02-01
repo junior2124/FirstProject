1cf4843eb0664089dcabb1b2664a3c25c48bf4db
junior2124 <junior2124@msn.com>
2018-01-31 22:57:29 -0600

    Add References to project.

---
+++ FirstGitProject/Controllers/HomesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstGitProject.Models;
+
+namespace FirstGitProject.Controllers
+{
+    public class HomesController : Controller
+    {
+        // GET: Homes/Random
+        public ActionResult Random()
+        {
+            var home = new Home { Address = "2124 N Menard"};
+
+            return View(home);
+        }
+    }
+}
---
+++ FirstGitProject/Models/Home.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstGitProject.Models
+{
+    public class Home
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        public int Beds { get; set; }
+
+        public int Baths { get; set; }
+
+        public int HomeSize { get; set; }
+
+        public int LotSize { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
